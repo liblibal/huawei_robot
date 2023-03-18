7ee10981f6f784a4cc010865cefdf3a134651062
PM25000 <1261859347@qq.com>
2023-03-18 15:40:10 +0800

    Update main.cpp

--- SDK/c++/main.cpp
+++ SDK/c++/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+const int maxn=1024;
+
+
+
+
 
 bool initreadUntilOK() {
     char line[1024];
+    int now=0;
     while (fgets(line, sizeof line, stdin)) {
         if (line[0] == 'O' && line[1] == 'K') {
             return true;
         }
-        //do something
     }
     return false;
 }
@@ -19,8 +27,66 @@ bool readUntilOK() {
             return true;
         }
         //do something
+        
     }
     return false;
+}
+
+int frameID;
+int money;
+int bench_cnt;
+class bench{
+    int type;
+    double x,y;
+    int last;
+    int instatus;
+    int outstatus;
+}
+bench benchs[55];
+
+string instr[5]={
+    "forward",
+    "rotate",
+    "buy",
+    "sell",
+    "destroy"
+};
+class robot{
+    int benchid;
+    int carry;
+    double time_factor;
+    double bump_factor;
+
+    vector<int> 
+}
+bool readframe()
+{
+    scanf("%d",&frameID);
+    scanf("%d",&money);
+    scanf("%d",&bench_cnt);
+    for(int i=0;i<bench_cnt;i++)
+    {
+        scanf("%d",&benchs[i].type);
+        scanf("%lf",&benchs[i].x);
+        scanf("%lf",&benchs[i].y);
+        scanf("%d",&benchs[i].last);
+        scanf("%d",&benchs[i].instatus);
+        scanf("%d",&benchs[i].outstatus);
+    }
+    scanf("%d",&robot_cnt);
+    for(int i=0;i<robot_cnt;i++)
+    {
+        scanf("%d",&robots[i].benchid);
+        scanf("%d",&robots[i].carry);
+        scanf("%lf",&robots[i].time_factor);
+        scanf("%lf",&robots[i].bump_factor);
+    }
+    return true;
+}
+
+bool move_to_bench(int robotid,int benchid)
+{
+
 }
 
 int main() {
