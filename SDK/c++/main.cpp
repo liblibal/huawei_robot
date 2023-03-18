95f35d52da79544e788fe92a78fbb0afbcaaf6b4
PM25000 <1261859347@qq.com>
2023-03-18 14:47:21 +0800

    1

--- SDK/c++/main.cpp
+++ SDK/c++/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+bool initreadUntilOK() {
+    char line[1024];
+    while (fgets(line, sizeof line, stdin)) {
+        if (line[0] == 'O' && line[1] == 'K') {
+            return true;
+        }
+        //do something
+    }
+    return false;
+}
+
 bool readUntilOK() {
     char line[1024];
     while (fgets(line, sizeof line, stdin)) {
@@ -13,7 +24,7 @@ bool readUntilOK() {
 }
 
 int main() {
-    readUntilOK();
+    initreadUntilOK();
     puts("OK");
     fflush(stdout);
     int frameID;
